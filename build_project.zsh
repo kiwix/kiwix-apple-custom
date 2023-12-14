cd9c993255457e10f9224eed6f49aeb0a8ccdd52
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2023-12-14 22:38:47 +0100

    Move about text as well

--- build_project.zsh
+++ build_project.zsh
@@ -20,6 +20,7 @@ do
     url=`jq .zim_url -r $info`
     auth=`jq .zim_auth -r $info`
     about_url=`jq .about_app_url -r $info`
+    about_text=`jq .about_text -r $info`
     
     parent_url=${url%/*}
     file_name=${url:${#parent_url} + 1} # + 1 to remove the trailing slash
@@ -28,6 +29,7 @@ do
     curl -O -L $url -u "$auth_value" $parent_dir/$file_name
     echo "CUSTOM_ZIM_FILE = "${file_name%.zim}"\n" > apple/$brand_name/$brand_name.xcconfig
     echo "CUSTOM_ABOUT_WEBSITE = "${about_url}"\n" > apple/$brand_name/$brand_name.xcconfig
+    echo "CUSTOM_ABOUT_TEXT = "${about_text}"\n" > apple/$brand_name/$brand_name.xcconfig
 done
 
 # download libkiwix xcframework
