283e8319ee18a796d823e399382d7775aa050721
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2023-12-15 09:47:42 +0100

    Update build script

--- build_project.zsh
+++ build_project.zsh
@@ -4,13 +4,13 @@
 lib_kiwix_framework="libkiwix_xcframework-13.0.0-1"
 
 # move the custom files under the same folder as the kiwix repo
-mv custom/ apple/Support/
+mv custom/ apple/
 cd apple
 
 #download custom zim files as per info.json files
 brew install jq
 
-for info in `mdfind -onlyin . -name info.json`
+for info in `mdfind -onlyin apple/custom/ -name info.json`
 do
     parent_dir=${info%/*}
     brand_name=${parent_dir##*/}
@@ -26,9 +26,9 @@ do
 
     auth_value=`print -rl -- ${(P)auth}` # get the credentials from environment var named by .zim_auth in the json
     curl -O -L $url -u "$auth_value" $parent_dir/$file_name
-    echo "CUSTOM_ZIM_FILE = "${file_name%.zim}"\n" > apple/$brand_name/$brand_name.xcconfig
-    echo "CUSTOM_ABOUT_WEBSITE = "${about_url}"\n" > apple/$brand_name/$brand_name.xcconfig
-    echo "CUSTOM_ABOUT_TEXT = "${about_text}"\n" > apple/$brand_name/$brand_name.xcconfig
+    echo "CUSTOM_ZIM_FILE = "${file_name%.zim}"\n" > custom/$brand_name/$brand_name.xcconfig
+    echo "CUSTOM_ABOUT_WEBSITE = "${about_url}"\n" > custom/$brand_name/$brand_name.xcconfig
+    echo "CUSTOM_ABOUT_TEXT = "${about_text}"\n" > custom/$brand_name/$brand_name.xcconfig
 done
 
 # download libkiwix xcframework
@@ -37,6 +37,7 @@ curl -O -L $lib_kiwix_url
 tar xzf $lib_kiwix_framework".tar.gz"
 rm $lib_kiwix_framework".tar.gz"
 mv libkiwix_xcframework*/lib/*.* .
+rm -rf libkiwix_xcframework*/
 
 ls -la
 
