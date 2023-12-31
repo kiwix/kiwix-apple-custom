bb1b868d71b0e56666c3c4cbbfcb8ca3fbd94e63
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2023-12-31 10:59:21 +0100

    Update the build steps to use python

--- build_project.zsh
+++ build_project.zsh
@@ -4,44 +4,15 @@
 mv custom/ apple/
 cd apple/custom
 
-# download custom zim files as per info_local.json files
-# and configure the project.yml file accordingly
-for info in `python src/find_infos.py`
-do
-    parent_dir=${info%/*}
-    brand_name=${parent_dir##*/}
-    echo "brand_name: " $brand_name
+# make a custom copy of the Info.plist file
+cp ../Support/Info.plist Custom.plist
 
-    url=`jq .zim_url -r $info`
-    auth=`jq .zim_auth -r $info`
-    
-    parent_url=${url%/*}
-    file_name=${url:${#parent_url} + 1} # + 1 to remove the trailing slash
+# download all the zim files, 
+# generate all the branded config files, 
+# generate the custom project file containing all brands as targets
+python "src/generate_and_download.py"
 
-    auth_value=`print -rl -- ${(P)auth}` # get the credentials from environment var named by .zim_auth in the json
-    curl -L $url -u "$auth_value" -o $parent_dir/$file_name
-
-    touch $brand_name/$brand_name.xcconfig
-    python src/configure.py --output xcconfig $info > $brand_name/$brand_name.xcconfig
-    echo "app name"
-    python src/configure.py --output app_name $info
-    echo "app version"
-    python src/configure.py --output app_version $info
-    echo "enforced language"
-    python src/configure.py --output enforced_language $info
-
-    # copy the Info.plist file to custom folder
-    cp ../Support/Info.plist $brand_name/$brand_name.plist
-
-    # TODO: do this only once as Custom.plist, then just copy to the $brand folder as above
-    /usr/libexec/PlistBuddy -c "Add :CUSTOM_ZIM_FILE string \$(CUSTOM_ZIM_FILE)" $brand_name/$brand_name.plist
-    /usr/libexec/PlistBuddy -c "Add :CUSTOM_ABOUT_TEXT string \$(CUSTOM_ABOUT_TEXT)" $brand_name/$brand_name.plist
-    /usr/libexec/PlistBuddy -c "Add :CUSTOM_ABOUT_WEBSITE string \$(CUSTOM_ABOUT_WEBSITE)" $brand_name/$brand_name.plist
-    /usr/libexec/PlistBuddy -c "Add :SETTINGS_DEFAULT_EXTERNAL_LINK_TO string \$(SETTINGS_DEFAULT_EXTERNAL_LINK_TO)" $brand_name/$brand_name.plist
-    /usr/libexec/PlistBuddy -c "Add :SETTINGS_SHOW_EXTERNAL_LINK_OPTION string \$(SETTINGS_SHOW_EXTERNAL_LINK_OPTION)" $brand_name/$brand_name.plist
-    /usr/libexec/PlistBuddy -c "Add :SETTINGS_SHOW_SEARCH_SNIPPET string \$(SETTINGS_SHOW_SEARCH_SNIPPET)" $brand_name/$brand_name.plist
-done
-
+cp custom_project.yml ../
 cd ..
 ls -la
 
---
+++ src/generate_and_download.py
@@ -0,0 +1,22 @@
+"""Generate the custom app xcconfig, plist files, and a custom_project.yml. It is searching for info.json files in subfolders, where the subfolder name will become the "brand name" of the custom app."""
+
+from custom_apps import CustomApps
+
+if __name__ == "__main__":
+    custom_plist = "Custom.plist"
+    custom_apps = CustomApps()
+    
+    # first edit the plist file
+    custom_apps.append_to(custom_plist)
+    
+    # then copy it to the brand folders
+    custom_apps.copy_plist()
+    
+    # create the xcconfig files
+    custom_apps.create_xcconfigs()
+    
+    # download the zim files
+    custom_apps.download_zim_files()
+    
+    # finally create the project file, containing all brands as targets
+    custom_apps.create_custom_project_file()
