e30a8a127dfd9030bfdb0984b7e965161619f857
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2024-01-11 04:11:15 +0100

    Revert "Use pathlib"
    This reverts commit 5ef6d7f42bd813523887dc3a505ac1015a7a5f94.

--- src/info_parser.py
+++ src/info_parser.py
@@ -1,10 +1,10 @@
 from urllib.parse import urlparse
 import json
-from pathlib import Path
 import os
 import re
 import shutil
 import plistlib
+from glob import glob
 
 JSON_KEY_ZIM_URL = "zim_url"
 JSON_KEY_AUTH = "zim_auth"
@@ -23,15 +23,15 @@ JSON_TO_PLIST_MAPPING = {
 class InfoParser:
 
     def __init__(self, json_path):
-        self.brand_name = self._brandname_from(json_path)
-        content = json_path.read_text()
-        self.data = json.loads(content)
-        assert (JSON_KEY_ZIM_URL in self.data)
-        self.zim_file_name = self._filename_from(
-            self.data[JSON_KEY_ZIM_URL])
+        with open(json_path) as file:
+            self.brand_name = self._brandname_from(json_path)
+            self.data = json.loads(file.read())
+            assert (JSON_KEY_ZIM_URL in self.data)
+            self.zim_file_name = self._filename_from(
+                self.data[JSON_KEY_ZIM_URL])
 
     def create_plist(self, based_on_plist_file):
-        with based_on_plist_file.open(mode="rb") as file:
+        with open(based_on_plist_file, "rb") as file:
             plist = plistlib.load(file)
             for keyValues in self._plist_key_values():
                 for key in keyValues:
@@ -39,9 +39,10 @@ class InfoParser:
             plist[CUSTOM_ZIM_FILE_KEY] = self.zim_file_name
             out_path = self._info_plist_path()
             # create dir, if doesn't exists yet
-            if not out_path.parent.exists():
-                out_path.parent.mkdir()
-            with out_path.open(mode="wb") as out_file:
+            dirname = os.path.dirname(out_path)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname, exist_ok=True)
+            with open(out_path, "wb") as out_file:
                 plistlib.dump(plist, out_file)
 
     def as_project_yml(self):
@@ -81,15 +82,15 @@ class InfoParser:
         return self.data[JSON_KEY_ZIM_URL]
 
     def zim_file_path(self):
-        url = Path(self.zimurl())
-        return Path()/self.brand_name/url.name
+        url = self.zimurl()
+        return f"{self.brand_name}/{os.path.basename(url)}"
 
     def download_auth(self):
         auth_key = self.data[JSON_KEY_AUTH]
         return os.getenv(auth_key)
 
     def _info_plist_path(self):
-        return Path()/self.brand_name/f"{self.brand_name}.plist"
+        return f"{self.brand_name}/{self.brand_name}.plist"
 
     def _plist_key_values(self):
         for json_key in JSON_TO_PLIST_MAPPING:
@@ -119,10 +120,10 @@ class InfoParser:
             return None
 
     def _brandname_from(self, filepath):
-        return filepath.parent.name.lower()
+        return os.path.basename(os.path.dirname(filepath)).lower()
 
     def _filename_from(self, url):
-        return Path(urlparse(url).path).stem
+        return os.path.splitext(os.path.basename(urlparse(url).path))[0]
 
     def _app_version_from(self, file_name):
         p = re.compile('(?P<year>\d{4})-(?P<month>\d{1,2})')
@@ -142,9 +143,8 @@ class InfoParser:
             return ["**/qqq.lproj"]
         else:
             # Copy the enforced lang to the custom folder
-            for lang_file in Path().parent.rglob(f'{enforced}.lproj'):
-                lang_file.copy
+            for lang_file in glob(f'../**/{enforced}.lproj', recursive=True):
                 shutil.copytree(
-                    lang_file, Path().parent/"custom"/self.brand_name, dirs_exist_ok=True)
+                    lang_file, f"../custom/{self.brand_name}/", dirs_exist_ok=True)
             # exclude all other languages under Support/*.lproj
             return ["**/*.lproj"]
--- tests/custom_apps_test.py
+++ tests/custom_apps_test.py
@@ -1,23 +1,20 @@
 import unittest
 from src.custom_apps import CustomApps
-from pathlib import Path
-
 
 class CustomAppsTest(unittest.TestCase):
-
+    
     def setUp(self):
         self.custom = CustomApps()
-
+        
     def test_custom_plist(self):
-        self.custom.create_plists(
-            custom_plist=Path()/"tests"/"Support"/"Info.plist")
-
+        self.custom.create_plists(custom_plist="./tests/Support/Info.plist")
+        
     def test_custom_project_creation(self):
-        self.custom.create_custom_project_file(path=Path()/"custom_project_test.yml")
-
+        self.custom.create_custom_project_file(path="custom_project_test.yml")
+        
     def x_test_downloads(self):
         self.custom.download_zim_files()
-
+        
     def x_test_download_commands(self):
         for cmd in self.custom._curl_download_commands():
             print(cmd)
